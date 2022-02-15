9397b03825993e0010bcac3ae37b4d1193daf750
Youssef02222 <youssef.ibrahem.2022@gmail.com>
2022-02-15 14:29:52 +0200

    C13

---
+++ Sum.js
@@ -0,0 +1,10 @@
+function sum(...arr){
+    var result = 0;
+    for(var i = 0; i < arr.length; i++){
+        result += arr[i];
+    }
+    return result;
+}
+console.log(sum(5, 4));
+
+alert("welcome");
